1223285d072642497ecc7562ad318dca1c74ae75
Gabriel Hijazi <hijazi.root@gmail.com>
2023-10-10 15:14:06 -0400

    feat: create user controller

--- src/main/java/dev/gabrielhijazi/todolist/user/UserController.java
+++ src/main/java/dev/gabrielhijazi/todolist/user/UserController.java
@@ -1,23 +1,29 @@
 package dev.gabrielhijazi.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
- * Modifiers
- * PUBLIC - Qualquer um pode ter acesso
- * PRIVATE - Algumas restrições
- * PROTECTED - Deve estar na mesma estrutura do pacote
- */
-
 @RestController
 @RequestMapping("/users")
 public class UserController {
 
+  @Autowired
+  private IUserRepository userRepository;
+
   @PostMapping("/")
-  public void create(@RequestBody UserModel userModel) {
-    System.out.println(userModel.name);
+  public ResponseEntity create(@RequestBody UserModel userModel) {
+    var user = this.userRepository.findByUsername(userModel.getUsername());
+
+    if (user != null) {
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário ja existe!");
+    }
+
+    var userCreated = this.userRepository.save(userModel);
+    return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
   }
 }
